6b4fd0f91742374df693e8b063cded8344d1be10
Nick Ford <nickforddesign@gmail.com>
2019-03-06 11:31:42 -0500

    cleaning up install process

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+./scripts/brew      # runs bash
+./scripts/clone     # runs bash
+./scripts/install   # runs bash, this currently runs asynchronously...
+./scripts/link      # runs node
+./scripts/source    # runs zsh
