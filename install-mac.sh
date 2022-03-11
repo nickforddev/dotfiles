99ec82987da0d74402679b1beb6c8b819714ee9a
Nick Ford <nickforddesign@gmail.com>
2022-03-11 16:46:09 -0500

    fix remote install

--- install-mac.sh
+++ install-mac.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
 ./scripts/init      # runs bash
-./scripts/install   # runs bash
-./scripts/npm       # runs bash, this currently runs asynchronously...
-./scripts/link      # runs node
-./scripts/source    # runs zsh
