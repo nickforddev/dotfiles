d97028bcddfd7bfe5e6e5a4802246a0cdd8b75e4
Nick Ford <nichford@amazon.com>
2022-04-06 17:46:42 -0400

    add cava config

---
+++ configs/cava/config.sh
@@ -0,0 +1,29 @@
+[general]
+autosens = 1
+
+[input]
+source = "Soundflower (2ch)"
+
+[output]
+method = ncurses
+
+[color]
+; foreground = 'cyan'
+gradient = 1
+gradient_count = 8
+gradient_color_1 = '#50a6a8'
+gradient_color_2 = '#52B0B2'
+gradient_color_3 = '#53B9BB'
+gradient_color_4 = '#55C3C5'
+gradient_color_5 = '#57CDCF'
+gradient_color_6 = '#58D6D8'
+gradient_color_7 = '#5AE0E2'
+gradient_color_8 = '#5FFDFF'
+
+[eq]
+1=2
+2=2
+3=1
+4=1
+5=0.5
+
