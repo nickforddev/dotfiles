63d6b263c0c64566d8658059e44ba5d3d1427644
Nick Ford <nichford@amazon.com>
2022-08-18 12:06:23 -0400

    pywal improvements

--- configs/pywal/server.sh
+++ configs/pywal/server.sh
@@ -1,9 +1,19 @@
-PORT=2121
+WALSERVER_PORT=2121
 
 start_server() {
-  http-server ~/.cache/wal -p $PORT -c-1 -d false > /dev/null 2>&1 &
+  http-server ~/.dotfiles/configs/pywal -p $WALSERVER_PORT -c-1 -d false > /dev/null 2>&1 &
+}
+
+get_server_pid() {
+  lsof -ti:${WALSERVER_PORT}
 }
 
 kill_server() {
-  kill -9 $(lsof -ti:${PORT})
+  kill -9 $(get_server_pid)
 }
+
+# autostart
+get_server_pid > /dev/null 2>&1 || start_server
+
+alias walstart=start_server
+alias walstop=kill_server
--- configs/pywal/setup.sh
+++ configs/pywal/setup.sh
@@ -2,6 +2,7 @@ wal -i ~/.dotfiles/wallpaper/${1} --backend $2 --saturate $3 && \
   pywalfox update && \
   ~/.config/yabai/scripts/colors.sh && \
   ~/.dotfiles/configs/uebersicht/widgets/simple-bar/lib/styles/pywal/pywal-gen.sh && \
+  ~/.dotfiles/configs/alfred/alfred-pywal.sh && \
   ([ -f /Applications/IntelliJ\ IDEA.app ] && ~/.dotfiles/configs/jetbrains/intellijPywalGenMac.sh ~/Library/Preferences/IntelliJIdea2019.3) && \
   pkill -USR1 cava && \
   ~/.dotfiles/configs/pywal/slack.sh || true && \
